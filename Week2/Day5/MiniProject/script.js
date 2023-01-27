d0bfea84398f89e1f1a4ba873f690e0af4cb9162
pb180888 <pb180888@gmail.com>
2023-01-27 12:22:52 +0200

    added complited MiniProject of Week2

--- Week2/Day5/MiniProject/script.js
+++ Week2/Day5/MiniProject/script.js
@@ -1,27 +1,32 @@
 function playTheGame() {
     const userReady = confirm("Would you like to play game?");
 
-    if (!userReady) {
-        alert("No problem, Goodbye")
-        return
-    }
-
-    const answ = prompt("Type a number")
-
-    if (!IsonlyNumber(Number(answ))) {
-        alert("Sorry Not a number, Goodbye")
-        return
-    }
-
-    if (!inthemiddleZeroAndTen(answ)) {
-        alert("Sorry it’s not a good number, Goodbye")
-        return
-    }
+    if (!userReady) return alert("No problem, Goodbye")
 
     const computerNumber = generateRandomNumber()
-    console.log('Computer number:', computerNumber)
+    let answ = prompt("Type a number")
+    let numberOfTries = 0;
 
+    while (Number(answ) != computerNumber) {
+        numberOfTries = numberOfTries + 1;
+        if (numberOfTries === 3) return alert("Too many tries. The number was: " + computerNumber)
+        if (!IsonlyNumber(answ)) return alert("Sorry Not a number, Goodbye")
+        if (!inthemiddleZeroAndTen(Number(answ))) return alert("Sorry it’s not a good number, Goodbye")
+
+        console.log(Number(answ), computerNumber)
+
+        if (Number(answ) > computerNumber) {
+            alert("Your number is bigger then the computer’s, guess again")
+        }
+        if (Number(answ) < computerNumber) {
+            alert("Your number is smaller then the computer’s, guess again")
+        }
+        answ = prompt("Type another number")
+    }
+
+    alert("Winner ! the number was indeed " + computerNumber + " You found in " + (numberOfTries + 1) + " tries")
 }
+
 
 
 function IsonlyNumber(str) {
@@ -35,3 +40,4 @@ function inthemiddleZeroAndTen(number) {
 function generateRandomNumber() {
     return Math.floor(Math.random() * 10)
 }
+
