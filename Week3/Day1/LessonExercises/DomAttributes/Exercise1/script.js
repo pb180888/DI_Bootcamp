a716f53fd65d06953203b79125122bdc08df9ef4
pb180888 <pb180888@gmail.com>
2023-01-29 23:53:20 +0200

    changed tree of folder

---
+++ Week3/Day1/LessonExercises/DomAttributes/Exercise1/script.js
@@ -0,0 +1,5 @@
+let newInput = document.getElementById("dI")
+console.log(newInput.getAttribute("type"));
+console.log(newInput.getAttribute("hreflang"));
+console.log(newInput.getAttribute("rel"));
+console.log(newInput.getAttribute("target"));
